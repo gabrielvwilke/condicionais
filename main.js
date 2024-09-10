54feb9a0dc7aba7f25e13c08a27ce8cbc1fe5a5a
Gabriel Veras Wilke <gabrielvwilke@gmail.com>
2024-09-09 21:29:29 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function questao1(){
+    let A = parseFloat
+    let B = parseFloat
+    let C = parseFloat(prompt("Digite o valor de C: ").replace(",",","));
+
+    if (A + B < C) {
+        
+    }
+}
