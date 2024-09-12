d306733a85249e2d57390393ac7e21a9892c9edf
Gabriel Veras Wilke <gabrielvwilke@gmail.com>
2024-09-12 20:13:12 -0300

    segundo commit

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 function questao1(){
-    let A = parseFloat
-    let B = parseFloat
+    let A = parseFloat(prompt("Digite o valor de A: ").replace(",",","));
+    let B = parseFloat(prompt("Digite o valor de B: ").replace(",",","));
     let C = parseFloat(prompt("Digite o valor de C: ").replace(",",","));
 
     if (A + B < C) {
-        
+     resultado = ("A soma de A + B é menor que C");
+     document.getElementById("resultado1").innerHTML = resultado;
+    }
+    else{
+        resultado = ("A soma de A + B é maior que C");
+        document.getElementById("resultado1").innerHTML = resultado;
+    }
+}   
+
+function questão2() {
+    let nome = prompt("Digite seu nome: ").toUpperCase();
+    let sexo = prompt("Digite (M)asculino ou (F)eminino: ");
+    let estadoCivil = prompt("Digite seu estado civil:").toUpperCase();
+    if (sexo ==="F" && estadoCivil === "CASADA") {
+        let tempoCasada = prompt("Digite o tempo de casada (anos): ");
+        resultado - 'Nome: ${nome}
+        Sexo: ${sexo}
+        EstadoCivil ${estadoCivil}
+        TempoDeCasada ${tempoCasada}';
     }
 }
